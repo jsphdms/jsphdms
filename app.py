482a5edaaf2edf734370312b12cb569cdc439946
Joseph Adams <joseph@josephadams.net>
2019-07-28 21:42:17 +0100

    Add some dummy posts

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+            {
+                'author': 'Joseph Adams',
+                'title': 'Here is a title',
+                'content': 'This is the content of the post',
+                'date_posted': '2019-07-30'
+                },
+            {
+                'author': 'Joe Bloggs',
+                'title': 'Another title',
+                'content': 'This is some more content',
+                'date_posted': '2019-07-31'
+                }
+        ]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 if __name__ == '__main__':
 	app.run(debug=True)
