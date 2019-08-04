16f3976eaaa7df9fd51f2a3ae71d0a17d98f519d
Joseph Adams <joseph@josephadams.net>
2019-08-04 22:06:21 +0100

    Add a post template

--- app.py
+++ app.py
@@ -20,5 +20,9 @@ posts = [
 def home():
     return render_template('home.html', posts = posts)
 
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
